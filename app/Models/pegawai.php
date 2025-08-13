aa4a23c4f9781ada3a05528d9daca7fcb5b47c02
MuhamadNaufalAL <naufal.nadukar@gmail.com>
2025-08-13 16:28:40 +0700

    initial commit

--- app/Models/pegawai.php
+++ app/Models/pegawai.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class pegawai extends Model
+class Pegawai extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $primaryKey = 'id_pegawai';
+
+    protected $fillable = [
+        'nama_pegawai',
+        'alamat',
+        'no_hp',
+        'jobdesk',
+        'password',
+        'email',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
 }
